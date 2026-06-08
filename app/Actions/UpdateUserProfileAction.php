104a0c69eddc6686a503d147205c1f293d388d0b
Vladyslav Shcherbyna <158246943+Vladyslav1728@users.noreply.github.com>
2026-06-08 17:56:46 +0200

    improved: update profile

--- app/Actions/UpdateUserProfileAction.php
+++ app/Actions/UpdateUserProfileAction.php
@@ -2,15 +2,35 @@
 
 namespace App\Actions;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+
 use App\Models\User;
 
 class UpdateUserProfileAction
 {
     public function execute(User $user, array $data): bool
     {
-        // Можно добавить дополнительные проверки если нужно
-        return $user->update([
-            'name' => trim($data['name'])
-        ]);
+        if (isset($data['name'])) {
+            $user->name = trim($data['name']);
+        }
+
+        if (array_key_exists('surname', $data)) {
+            $user->surname = $data['surname'] ? trim($data['surname']) : null;
+        }
+
+        if (isset($data['email']) && $data['email'] !== $user->email) {
+            $user->email = $data['email'];
+            $user->email_verified_at = null;
+        }
+
+        if (isset($data['avatar']) && $data['avatar'] instanceof UploadedFile) {
+            if ($user->avatar_path) {
+                Storage::disk('public')->delete($user->avatar_path);
+            }
+            $path = $data['avatar']->store('profile_photos', 'public');
+            $user->avatar_path = $path;
+        }
+        return $user->save();
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,18 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+// Запуск секундомера (Сколько секунд ушло на конкретный запрос)
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Если сайт закрыт (есть файл maintenance.php), запрос будет отклонён а сайт покажет "Сайт на обслуживании"
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Подключение autoload.php чтобы Laravel понимал местонахождение всех файлов (Контроллеры, Модели...)
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Подключение bootstrap/app.php
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// capture() берёт инф. HTTP-запроса, упаковывает в Request обьект, передаёт в routes/api, ищет нужный Controller
 $app->handleRequest(Request::capture());
